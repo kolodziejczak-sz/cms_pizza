f60a4a40bbd4c73f43473c4158b05b421d78ae4e
szymon <s.kolodziejczak95@gmail.com>
2018-11-25 21:16:14 +0100

    base layout

--- ad/views.py
+++ ad/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+def list(request):
+  list = [] #Ad.objects.order_by('-pub_date')
+  return render('ad/list.html', {
+    'list': list
+    'title': 'Announcements'
+  })
+
+def item(request, id):
+  item = { 'title': 'texttexttext', 'text':'texttext'} #get_object_or_404(Ad, pk=id)
+  return render('ad/item.html', {
+    'item': item
+  })
+
+def latest(request):
+  list = []#Ad.objects.order_by('-pub_date')[:5]
+  return render('ad/list.html', {
+    'list': list,
+    'title': 'Latest announcements'
+  })
