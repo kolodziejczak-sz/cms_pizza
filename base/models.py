5d7e6ae8f5f16d722532ffa4b4e1ddf3b0a148e1
szymon <s.kolodziejczak95@gmail.com>
2018-11-28 09:10:45 +0100

    nav & subsite model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from tinymce import models as tinymce_models
+from positions.fields import PositionField
 
-# Create your models here.
+class Subsite(models.Model):
+  title = models.CharField(max_length=200)
+  content = tinymce_models.HTMLField()
+  pub_date = models.DateTimeField('date published')
+
+class NavigationItem(models.Model):
+  label = models.CharField(max_length=50)
+  subsite = models.ForeignKey(Subsite, unique=True, on_delete=models.DO_NOTHING)
+  sort = PositionField(collection='subsite')
+  
+
+
